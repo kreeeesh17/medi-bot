b71bc9748688dc53ec52cc62aaf70efbd43aa1f3
unknown <kreeshmodi.codeforces@gmail.com>
2025-06-22 02:12:48 +0530

    project created

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'Medical Query Response Chat Bot',
+    version = '0.0.0',
+    author = 'Kreesh Modi',
+    author_email = 'kreeshmodi2018@gmail.com',
+    packages = find_packages(),
+    install_requires = []
+)
+
